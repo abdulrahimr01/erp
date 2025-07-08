a975fc44f8e10481aeed1c2176891935eec0c744
Abdul Rahim <rahimrasak01@gmail.com>
2025-07-08 13:23:28 +0530

    New project added

---
+++ src/erp_vs/ipog.erp/Models/Employee.cs
@@ -0,0 +1,10 @@
+namespace ipog.erp.Models
+{
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+    }
+}
