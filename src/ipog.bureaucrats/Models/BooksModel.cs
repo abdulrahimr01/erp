b5258668cebfbbfa38da589dd66f39d3cb655510
jeyaganeshaj <jeyaganesharumugam21@gmail.com>
2025-08-14 12:47:37 +0530

    modal changed

--- src/ipog.bureaucrats/Models/BooksModel.cs
+++ src/ipog.bureaucrats/Models/BooksModel.cs
@@ -4,7 +4,7 @@ namespace ipog.bureaucrats.Models
     {
         public long Id { get; set; }
         public string? Title { get; set; }
-        public string? Name { get; set; }
+        public string? ExamName { get; set; }
         public string? Author { get; set; }
         public string? Price { get; set; }
         public string? Originalprice { get; set; }
@@ -13,9 +13,10 @@ namespace ipog.bureaucrats.Models
         public string? Stocks { get; set; }
         public bool IsActive { get; set; }
         public string? ActionBy { get; set; }
-        public DateTime? ActionDate { get; set; }
+        public DateTime? ActionDate { get; set; } 
         public byte[]? FrontImage { get; set; }
         public byte[]? BackImage { get; set; }
+        public string? Course {get;set;}
     }
 
     public class GetBooksModel : BooksModel
